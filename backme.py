c4128f76de7dce322c55df69c552dda80f80ce06
prabhu-are <flyingcolours47@gmail.com>
2014-05-12 16:32:57 +0530

    second veriosn with .ini file

--- backme.py
+++ backme.py
@@ -4,76 +4,62 @@ from ConfigParser import SafeConfigParser
 from datetime import datetime
 import calendar
 import tarfile,os,glob,string,subprocess,time,sys
-from logging import handlers
+import logging
 
-today = time.strftime("%Y-%m-%d")
-backup_date = today
-DEVNULL = open(os.devnull, 'wb')
+Current_date_time = datetime.now().strftime("%Y-%m-%d") + "-" + str(calendar.timegm(time.gmtime())) 
+
+LOGGER = logging.getLogger('BackMe')
+LOG = logging.FileHandler('BackMe.log')
+LOGGER.setLevel(logging.INFO)
+
+FORMAT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOG.setFormatter(FORMAT)
+
+LOGGER.addHandler(LOG)
 
 parser = SafeConfigParser()
-parser.read('archive_config.ini')
-
+parser.read ('archive_config.ini')
 main_directory =  parser.get('basic_conf', 'parent_directory')
-depth  = parser.get('basic_conf', 'depth')
-modified = parser.get('basic_conf', 'modified')
-tar_location = parser.get('basic_conf', 'tar_location')
-
-if not os.path.exists(tar_location):
-        os.makedirs(tar_location)
+depth  = parser.get ('basic_conf', 'depth')
+modified = parser.get ('basic_conf', 'modified')
+tar_location = parser.get ('basic_conf', 'tar_location')
+host_name = parser.get ('basic_conf', 'host_ip')
+path_name = parser.get ('basic_conf','tarsave_path')
+user_name = parser.get ('basic_conf','use_name')
 rootDir = main_directory
 
 def process_execute(command):
-        proc = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE)
-        output,error = proc.communicate()
-        return output,error
+        proc = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+       	output,error = proc.communicate()
+	return output,error	
 
-def get_disk_size(folder):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(folder):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
+find_list_cmd = "find "+ rootDir +" -maxdepth "+depth+" -mindepth "+depth+" -type d -mtime " + modified
 
-def split_the_path(path):
-  allparts = []
-    while 1:
-        parts = os.path.split(path)
-        if parts[0] == path:
-            allparts.insert(0, parts[0])
-            break
-        elif parts[1] == path:
-            allparts.insert(0, parts[1])
-            break
-        else:
-            path = parts[0]
-            allparts.insert(0, parts[1])
-    return allparts
-
-# tar tzvf 19-03-2014-po.tar | awk '{i+=$3} END{print (i/1024/1024) "MB"}'
-find_list_cmd = "find "+ rootDir +" -maxdepth "+depth+" -mindepth "+depth+"  -type d -mtime "+ "+" + modified
-(compute_out, compute_err) = process_execute(find_list_cmd)
+LOGGER.info('PREPARING THE TAR WITH %s' % (find_list_cmd))
+compute_out = process_execute(find_list_cmd)
 
 if compute_out == "":
+	LOGGER.info('TAR FAILS, THERE IS NO FILE IN TAR ')
         print "Sorry No folders exists at the given search location"
         sys.exit(3)
-folders_to_tar =  compute_out.rstrip()
 
-#print folders_to_tar
+folders_to_tar = compute_out[0].rstrip('')
+
 l = folders_to_tar.split('\n')
-for folders in l:
-        folder_size = get_disk_size(folders)
-        ls_command = "ls -al "+ folders
-        ls_output,ls_error = process_execute(ls_command)
-        client_name = os.path.basename(folders)
-        paths = split_the_path(folders)
-        working_folder = paths[-2]
-        archive_name = working_folder+ "-" +client_name+ "-" + datetime.now().strftime("%Y-%m-%d") + "-"+str(calendar.timegm(time.gmtime()))  + ".tar"
-        archive_full_path = tar_location+ "/"+ archive_name
-        print "archiving "+folders+ " at "+ archive_full_path
-        text_file_name = folders+"/"+archive_name+ ".txt"
-        ls_output_file = open(text_file_name, "w")
-        ls_output_file.write(ls_output)
-        ls_output_file.close()
-        tar_command = "tar --absolute-names -zcvf  "+archive_full_path+ " " +folders+ " --remove-files"
-        tar_output,tar_error = process_execute(tar_command)
+
+tar = tarfile.open('%s/BackMe_%s.tgz' % (tar_location,Current_date_time),'w:gz')
+
+LOGGER.info ('PREPARING THE TAR')
+
+for name in l[0:-1]:
+	print name
+	tar.add(name)
+tar.close()	
+
+LOGGER.info ('TAR IS DONE WITH THE NAME BackMe_%s IN THE LOCATION %s' % (Current_date_time,tar_location))
+
+LOGGER .info ('STARTING THE SCP PROCESS')
+
+process_execute('scp %s/BackMe_%s.tgz %s@%s:%s' % (tar_location,Current_date_time,user_name,host_name,path_name))
+
+LOGGER.info ('SCP IS DONE WITH THE USERNAME %s AND THE HOSTNAME %s ON THE REMOTE LOCATION %s'  % (user_name,host_name,path_name))
